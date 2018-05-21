13805166b82a9a179cd5c7a6a23fabcd2b524780
Malcolm Lockyer <segfault88@gmail.com>
2018-05-21 21:37:02 +1200

    FACEPALM!

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,18 @@
 #[macro_use]
 extern crate glium;
+
 extern crate image;
 
-use std::io::Cursor;
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+    tex_coords: [f32; 2],
+}
+
+implement_vertex!(Vertex, position, tex_coords);
 
 fn main() {
     use glium::{glutin, Surface};
-
-    let mut events_loop = glutin::EventsLoop::new();
-    let window = glutin::WindowBuilder::new();
-    let context = glutin::ContextBuilder::new();
-    let display = glium::Display::new(window, context, &events_loop).unwrap();
-
-    let image = image::load(
-        Cursor::new(&include_bytes!("/Users/malcolm/Projects/rust/rscraft/assets/kitty.jpg")[..]),
-        image::PNG,
-    ).unwrap()
-        .to_rgba();
-    let image_dimensions = image.dimensions();
-    let image =
-        glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
-    let texture = glium::texture::Texture2d::new(&display, image).unwrap();
-
-    #[derive(Copy, Clone)]
-    struct Vertex {
-        position: [f32; 2],
-        tex_coords: [f32; 2],
-    }
-
-    implement_vertex!(Vertex, position, tex_coords);
 
     let vertex1 = Vertex {
         position: [-0.5, -0.5],
@@ -44,46 +28,63 @@ fn main() {
     };
     let shape = vec![vertex1, vertex2, vertex3];
 
-    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-
     let vertex_shader_src = r#"
-        #version 140
-        in vec2 position;
-        in vec2 tex_coords;
-        out vec2 v_tex_coords;
-        uniform mat4 matrix;
-        void main() {
-            v_tex_coords = tex_coords;
-            gl_Position = matrix * vec4(position, 0.0, 1.0);
-        }
-    "#;
+#version 140
+
+in vec2 position;
+in vec2 tex_coords;
+out vec2 v_tex_coords;
+
+uniform mat4 matrix;
+
+void main() {
+    v_tex_coords = tex_coords;
+    gl_Position = matrix * vec4(position, 0.0, 1.0);
+}
+"#;
 
     let fragment_shader_src = r#"
-        #version 140
-        in vec2 v_tex_coords;
-        out vec4 color;
-        uniform sampler2D tex;
-        void main() {
-            color = texture(tex, v_tex_coords);
-        }
-    "#;
+#version 140
 
+in vec2 v_tex_coords;
+out vec4 color;
+
+uniform sampler2D tex;
+
+void main() {
+    color = texture(tex, v_tex_coords);
+}
+"#;
+
+    use std::io::Cursor;
+    let image = image::load(
+        Cursor::new(&include_bytes!("/Users/malcolm/Projects/rust/rscraft/assets/kitty.jpg")[..]),
+        image::JPEG,
+    ).unwrap()
+        .to_rgba();
+    let image_dimensions = image.dimensions();
+    let image =
+        glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
+
+    let mut events_loop = glium::glutin::EventsLoop::new();
+    let window = glium::glutin::WindowBuilder::new();
+    let context = glium::glutin::ContextBuilder::new();
+    let display = glium::Display::new(window, context, &events_loop).unwrap();
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
     let program =
         glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None)
             .unwrap();
+    let texture = glium::texture::Texture2d::new(&display, image).unwrap();
 
-    let mut t = -0.5;
+    let mut t: f32 = -10.0;
     let mut closed = false;
     while !closed {
-        // we update `t`
-        t += 0.0002;
-        if t > 0.5 {
-            t = -0.5;
+        t += 0.004;
+        if t > 10.0 {
+            t = -10.0;
         }
-
-        let mut target = display.draw();
-        target.clear_color(0.0, 0.0, 1.0, 1.0);
 
         let uniforms = uniform! {
             matrix: [
@@ -95,6 +96,8 @@ fn main() {
             tex: &texture,
         };
 
+        let mut target = display.draw();
+        target.clear_color(0.1, 0.1, 0.15, 1.0);
         target
             .draw(
                 &vertex_buffer,
@@ -106,7 +109,7 @@ fn main() {
             .unwrap();
         target.finish().unwrap();
 
-        events_loop.poll_events(|event| match event {
+        events_loop.poll_events(|ev| match ev {
             glutin::Event::WindowEvent { event, .. } => match event {
                 glutin::WindowEvent::Closed => closed = true,
                 _ => (),
