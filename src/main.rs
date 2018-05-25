5ff8b20904b92c79bf549b7760b1aae3d49084ee
Malcolm Lockyer <segfault88@gmail.com>
2018-05-25 19:31:43 +1200

    some specular

--- src/main.rs
+++ src/main.rs
@@ -41,12 +41,13 @@ fn main() {
     ).unwrap();
 
     let vertex_shader_src = r#"
-#version 150
+#version 330
 
 in vec3 position;
 in vec3 normal;
 
 out vec3 v_normal;
+out vec3 v_position;
 
 uniform mat4 matrix;
 uniform mat4 perspective;
@@ -54,21 +55,32 @@ uniform mat4 perspective;
 void main() {
     v_normal = transpose(inverse(mat3(matrix))) * normal;
     gl_Position = perspective * matrix * vec4(position, 1.0);
+    v_position = gl_Position.xyz / gl_Position.w;
 }
     "#;
 
     let fragment_shader_src = r#"
-#version 150
+#version 330
 
 in vec3 v_normal;
+in vec3 v_position;
+
 out vec4 color;
+
 uniform vec3 u_light;
 
+const vec3 ambient_color = vec3(0.0, 0.0, 0.2);
+const vec3 diffuse_color = vec3(0.0, 0.0, 0.6);
+const vec3 specular_color = vec3(0.8, 0.8, 0.8);
+
 void main() {
-    float brightness = dot(normalize(v_normal), normalize(u_light));
-    vec3 dark_color = vec3(0.0, 0.0, 0.6);
-    vec3 regular_color = vec3(0.0, 0.0, 1.0);
-    color = vec4(mix(dark_color, regular_color, brightness), 1.0);
+    float diffuse = max(dot(normalize(v_normal), normalize(u_light)), 0.0);
+
+    vec3 camera_dir = normalize(-v_position);
+    vec3 half_direction = normalize(normalize(u_light) + camera_dir);
+    float specular = pow(max(dot(half_direction, normalize(v_normal)), 0.0), 16.0);
+
+    color = vec4(ambient_color + diffuse * diffuse_color + specular * specular_color, 1.0);
 }
     "#;
 
