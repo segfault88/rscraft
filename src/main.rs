df125539403d7c44f43d448258dbf49b0531143b
Malcolm Lockyer <segfault88@gmail.com>
2018-05-25 19:51:10 +1200

    manual view matrix

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,46 @@ use std::io::Cursor;
 
 mod teapot;
 
+fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
+    let f = {
+        let f = direction;
+        let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
+        let len = len.sqrt();
+        [f[0] / len, f[1] / len, f[2] / len]
+    };
+
+    let s = [
+        up[1] * f[2] - up[2] * f[1],
+        up[2] * f[0] - up[0] * f[2],
+        up[0] * f[1] - up[1] * f[0],
+    ];
+
+    let s_norm = {
+        let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
+        let len = len.sqrt();
+        [s[0] / len, s[1] / len, s[2] / len]
+    };
+
+    let u = [
+        f[1] * s_norm[2] - f[2] * s_norm[1],
+        f[2] * s_norm[0] - f[0] * s_norm[2],
+        f[0] * s_norm[1] - f[1] * s_norm[0],
+    ];
+
+    let p = [
+        -position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
+        -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
+        -position[0] * f[0] - position[1] * f[1] - position[2] * f[2],
+    ];
+
+    [
+        [s_norm[0], u[0], f[0], 0.0],
+        [s_norm[1], u[1], f[1], 0.0],
+        [s_norm[2], u[2], f[2], 0.0],
+        [p[0], p[1], p[2], 1.0],
+    ]
+}
+
 fn main() {
     use glium::{glutin, Surface};
 
@@ -47,15 +87,15 @@ in vec3 position;
 in vec3 normal;
 
 out vec3 v_normal;
-out vec3 v_position;
 
-uniform mat4 matrix;
 uniform mat4 perspective;
+uniform mat4 view;
+uniform mat4 model;
 
 void main() {
-    v_normal = transpose(inverse(mat3(matrix))) * normal;
-    gl_Position = perspective * matrix * vec4(position, 1.0);
-    v_position = gl_Position.xyz / gl_Position.w;
+    mat4 modelview = view * model;
+    v_normal = transpose(inverse(mat3(modelview))) * normal;
+    gl_Position = perspective * modelview * vec4(position, 1.0);
 }
     "#;
 
@@ -63,24 +103,13 @@ void main() {
 #version 330
 
 in vec3 v_normal;
-in vec3 v_position;
-
 out vec4 color;
-
 uniform vec3 u_light;
-
-const vec3 ambient_color = vec3(0.0, 0.0, 0.2);
-const vec3 diffuse_color = vec3(0.0, 0.0, 0.6);
-const vec3 specular_color = vec3(0.8, 0.8, 0.8);
-
 void main() {
-    float diffuse = max(dot(normalize(v_normal), normalize(u_light)), 0.0);
-
-    vec3 camera_dir = normalize(-v_position);
-    vec3 half_direction = normalize(normalize(u_light) + camera_dir);
-    float specular = pow(max(dot(half_direction, normalize(v_normal)), 0.0), 16.0);
-
-    color = vec4(ambient_color + diffuse * diffuse_color + specular * specular_color, 1.0);
+    float brightness = dot(normalize(v_normal), normalize(u_light));
+    vec3 dark_color = vec3(0.6, 0.0, 0.0);
+    vec3 regular_color = vec3(1.0, 0.0, 0.0);
+    color = vec4(mix(dark_color, regular_color, brightness), 1.0);
 }
     "#;
 
@@ -100,11 +129,11 @@ void main() {
         let mut target = display.draw();
         target.clear_color_and_depth((0.1, 0.1, 0.2, 1.0), 1.0);
 
-        let matrix = [
+        let model = [
             [t.cos() * 0.01, t.sin() * 0.01, 0.0, 0.0],
             [-t.sin() * 0.01, t.cos() * 0.01, 0.0, 0.0],
             [0.0, 0.0, 0.01, 0.0],
-            [0.0, 0.0, 4.0 + t.cos() * 10.0, 1.0f32],
+            [0.0, 0.0, 2.0, 1.0f32],
         ];
 
         let perspective = {
@@ -138,12 +167,14 @@ void main() {
             ..Default::default()
         };
 
+        let view = view_matrix(&[2.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0]);
+
         target
             .draw(
                 (&positions, &normals),
                 &indices,
                 &program,
-                &uniform! { matrix: matrix, perspective: perspective, u_light: light },
+                &uniform! { model: model, view:view, perspective: perspective, u_light: light },
                 &params,
             )
             .unwrap();
