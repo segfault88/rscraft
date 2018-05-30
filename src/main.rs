4066ed32cf7f2a6bcc7d4a1cc513b1881d8361ed
Malcolm Lockyer <segfault88@gmail.com>
2018-05-30 20:12:12 +1200

    we're still inverted :|

--- src/main.rs
+++ src/main.rs
@@ -134,7 +134,7 @@ void main() {
         }
 
         let mut target = display.draw();
-        target.clear_color_and_depth((0.1, 0.1, 0.2, 1.0), 1.0);
+        target.clear_color_and_depth((0.1, 0.1, 0.2, 1.0), -9999999.0);
 
         // let model = [
         //     [t.cos() * 0.01, t.sin() * 0.01, 0.0, 0.0],
@@ -146,7 +146,7 @@ void main() {
         let rot = Matrix4::from_scaled_axis(&Vector3::x() * 3.14 * t);
 
         let model_matrix =
-            Matrix4::new_scaling(0.1).append_translation(&Vector3::new(1.0f32, t * 10.0, 50.0f32))
+            Matrix4::new_scaling(0.1).append_translation(&Vector3::new(-1.0f32, t * -10.0, -90.0f32))
                 * rot;
 
         let model: [[f32; 4]; 4] = model_matrix.into();
@@ -185,8 +185,8 @@ void main() {
         // let up: Vector3<f32> = Vector3::new(0f32, 0f32, 1f32);
         // let look_at_dir: Matrix4<f32> = Matrix4::look_at_dir(eye, dir, up);
 
-        let eye = Point3::new(-1.0, 0.0, -1.0);
-        let look_target = Point3::new(-1.0, 0.0, 0.0);
+        let eye = Point3::new(1.0, 0.0, 1.0);
+        let look_target = Point3::new(1.0, 0.0, 0.0);
         let naview = Matrix4::look_at_rh(&eye, &look_target, &Vector3::y());
 
         // the direction of the light
@@ -194,7 +194,7 @@ void main() {
 
         let params = glium::DrawParameters {
             depth: glium::Depth {
-                test: glium::draw_parameters::DepthTest::IfLess,
+                test: glium::draw_parameters::DepthTest::IfMore,
                 write: true,
                 ..Default::default()
             },
